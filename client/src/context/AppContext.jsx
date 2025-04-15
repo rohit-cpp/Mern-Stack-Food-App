54b0b2677a264962e3d4a6dce19dfb6e1f166d85
Rohit <rrgawanderohit@gmail.com>
2025-04-15 15:16:17 +0530

    All Products page and search feature done

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,6 +13,7 @@ export const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   const fetchProducts = async () => {
     setProducts(dummyProducts);
@@ -66,6 +67,8 @@ export const AppContextProvider = ({ children }) => {
     updateCartItem,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
