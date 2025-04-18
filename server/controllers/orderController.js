3a91f536e3b0b986241203716f8d7db4515afe8b
Rohit <rrgawanderohit@gmail.com>
2025-04-18 23:29:18 +0530

    order controller done

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -27,3 +27,35 @@ export const placeOrderCOD = async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+// get Orders by userId : /api/order/user
+
+export const getUserOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await Order.find({
+      userId,
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, orders });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// get All orders (for seller /admin) : /api/order/seller
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, orders });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
