8dbc9f282f4a992c6744cecd85ecb1c7ceaff6e3
Rohit <rrgawanderohit@gmail.com>
2025-04-18 17:33:12 +0530

    backend middlewares and controllers done

---
+++ server/controllers/orderController.js
@@ -0,0 +1,29 @@
+import Order from "../models/Order";
+import Product from "../models/Product.js";
+
+// place order COD : /api/order/cod
+export const placeOrderCOD = async (req, res) => {
+  try {
+    const { userId, items, address } = req.body;
+    if (!address || items.length === 0) {
+      return res.json({ success: false, message: "Invalid data" });
+    }
+    //calculate amount using Items
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+    //Add Tax Charge (2%)
+    amount += Math.floor(amount * 0.02);
+    await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "COD",
+    });
+    return res.json({ success: true, message: "Order Placed Successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -5,9 +5,14 @@ import connectDB from "./config/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
+import connectCloudinary from "./config/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoutes.js";
+import addressRouter from "./routes/addressRoute.js";
 const app = express();
 const port = process.env.PORT || 4000;
 await connectDB();
+await connectCloudinary();
 
 const allowedOrigins = ["http://localhost:5173"];
 
@@ -18,6 +23,10 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => res.send("Api is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
